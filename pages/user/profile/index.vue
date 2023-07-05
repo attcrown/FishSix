d4ced2bed614e94a9543976cc7d4009903ff8a4d
Arin Srithawin <autio-auto@hotmail.com>
2023-07-05 13:12:03 +0700

    update student header

--- pages/user/profile/index.vue
+++ pages/user/profile/index.vue
@@ -1,20 +1,51 @@
 <template>
-    <v-row>
-        <v-col>
-            <h2>ข้อมูลนักเรียน</h2>
-            <v-avatar :size=iconSize>
-                <img :src=profilePics alt="John">
-            </v-avatar>
+    <div class="container-fluid">
+        <div class="col d-flex justify-content-center">
+            <v-row align="center">
+                <center>
+                    <div style="max-width: 500px;">
+                        <h1 class="font-weight-bold">ข้อมูลนักเรียน</h1>
+                    </div>
+                </center>
+                <div class="text-center">
+                    <v-col cols="12">
+                        <div>
+                            <v-avatar :size=iconSize>
+                                <img :src=profilePics alt="John">
+                            </v-avatar>
+                        </div>
+                    </v-col>
+                </div>
+                <div class="text-center">
+                    <v-col cols="12">
+                        <strong class="header">&nbsp;นาย อริน สลับสี{{ firstName }} {{ keyuser }}
+                        </strong> <br>
+                        <span class="userSpan">ผู้เรียน</span>
+                    </v-col>
+                </div>
+                <v-col cols="12">
+                    <v-card>
+                        <v-card-title class="font-weight-bold header justify-center">ชั่วโมงเรียนเหลือ</v-card-title>
+                        <v-card-text>
+                            <v-row align="center">
+                                <v-col class="text-h2 text-center" cols="4">
+                                    23&deg;C
+                                    <p>Fliped class</p>
+                                </v-col>
+                                <v-col class="text-h2 text-center" cols="4">
+                                    23&deg;C
+                                </v-col>
+                                <v-col class="text-h2 text-center" cols="4">
+                                    23&deg;C
+                                </v-col>
+                            </v-row>
+                        </v-card-text>
+                    </v-card>
+                </v-col>
+            </v-row>
 
-            <strong>
-                <div class="text-h5 mt-2">{{ firstName }} {{ keyuser }}</div>
-            </strong>
-
-
-
-        </v-col>
-        <v-col></v-col>
-    </v-row>
+        </div>
+    </div>
 </template>
 
 
@@ -24,7 +55,7 @@ export default {
     data() {
         return {
             profilePics: "https://cdn.vuetifyjs.com/images/john.jpg",
-            iconSize: 100,
+            iconSize: 120,
         }
     },
 
@@ -49,3 +80,17 @@ export default {
     },
 }
 </script>
+
+<style>
+.userSpan {
+    background-color: rgb(243 244 246);
+    color: #000000;
+    border-radius: 12px;
+    padding: 4px 10px 4px 10px;
+
+}
+
+.header {
+    font-size: 25px;
+}
+</style>
