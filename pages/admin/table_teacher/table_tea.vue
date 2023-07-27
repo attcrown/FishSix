a674a896561ba625fad1912c1634cc563448345d
attcrwon <ittatting@gmail.com>
2023-07-27 14:11:27 +0700

    upgrade_match_table_tea

--- pages/admin/table_teacher/table_tea.vue
+++ pages/admin/table_teacher/table_tea.vue
@@ -98,11 +98,12 @@
                                     </v-col>
                                     <v-col cols="12" sm="4">
                                         <v-select :items="time_standart" v-model="picker_start" label="เริ่มสอน"
-                                            @change="validateTime(), picker_stop=null"></v-select>
+                                            @change="validateTime(), picker_stop = null"
+                                            :disabled="mode == 'edit'"></v-select>
                                     </v-col>
                                     <v-col cols="12" sm="4">
                                         <v-select :items="time_standart_stop" v-model="picker_stop" @change="validateTime()"
-                                            label="หยุดสอน"></v-select>
+                                            label="หยุดสอน" :disabled="mode == 'edit'"></v-select>
                                     </v-col>
 
                                     <!-- <v-col cols="12" sm="4">
@@ -264,14 +265,14 @@ export default {
 
         hour_tea: 0,
         min_tea: 0,
-
-        time_standart: ["00:00", "00:30", "01:00", "01:30", "02:00", "02:30", "03:00"
+        time_full: ["00:00", "00:30", "01:00", "01:30", "02:00", "02:30", "03:00"
             , "03:30", "04:00", "04:30", "05:00", "05:30", "06:00", "06:30"
             , "07:00", "07:30", "08:00", "08:30", "09:00", "09:30", "10:00"
             , "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30"
             , "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00"
             , "17:30", "18:00", "18:30", "19:00", "19:30", "20:00", "20:30"
             , "21:00", "21:30", "22:00", "22:30", "23:00", "23:30"],
+        time_standart: [],
         time_standart_stop: [],
         time_standart_sum: [],
         picker_start: null,
@@ -323,7 +324,7 @@ export default {
 
     methods: {
         validateTime() {
-            if (true){//this.picker_stop == null) {
+            if (true) {//this.picker_stop == null) {
                 this.time_standart_stop = [];
                 let sum = 0;
                 for (const key in this.time_standart) {
@@ -337,12 +338,12 @@ export default {
                 }
             }
             if (this.picker_stop != null && this.picker_start != null) {
-                this.time_standart_sum=[];
+                this.time_standart_sum = [];
                 let sum = 0;
                 for (const key in this.time_standart) {
                     if (this.picker_stop == this.time_standart[key]) {
                         sum = 0;
-                        this.time_standart_sum.push(this.time_standart[key]);
+                        // this.time_standart_sum.push(this.time_standart[key]);
                         break;
                     }
                     else if (this.picker_start == this.time_standart[key] || (sum != 0)) {
@@ -351,7 +352,7 @@ export default {
                     }
                 }
                 console.log(this.time_standart_sum);
-            } 
+            }
             // else {
             //     alert("ลงเวลาไม่ถูกต้อง");
             //     this.close();
@@ -367,6 +368,8 @@ export default {
 
         save_detail_data() {
             // console.log(this.mode);
+            let id = new Date().getTime();
+            const db = this.$fireModule.database();
             if (this.save_detail.class == '' ||
                 this.save_detail.style == '' ||
                 this.save_detail.subject == '' ||
@@ -378,36 +381,91 @@ export default {
                 this.dialog_save_error = true;
                 return;
             }
-            const db = this.$fireModule.database();
-            if (this.mode == 'save') {
-                db.ref(`date_teacher/${this.value}/${this.date1}/${this.picker_stop}`).set({
-                    class: this.save_detail.class,
-                    style_subject: this.save_detail.style,
-                    sum_people: this.save_detail.sum_people,
-                    invite: '0',
-                    subject: this.save_detail.subject,
-                    start: this.picker_start,
-                    stop: this.picker_stop,
-                });
-            }
-            else if (this.mode == 'edit') {
-                if (this.delday != this.picker_stop) {
-                    db.ref(`date_teacher/${this.value}/${this.date1}/${this.delday}`).remove();
-                }
+            if (this.mode == 'edit') {
+                console.log('ทำๆ');
+                // if (this.delday != this.picker_stop) {
+                //     db.ref(`date_teacher/${this.value}/${this.date1}/${this.delday}`).remove();
+                // }
                 db.ref(`date_teacher/${this.value}/${this.date1}/${this.picker_stop}`).update({
                     class: this.save_detail.class,
                     sum_people: this.save_detail.sum_people,
                     subject: this.save_detail.subject,
                     style_subject: this.save_detail.style,
-                    start: this.picker_start,
-                    stop: this.picker_stop,
+                    // start: this.picker_start,
+                    // stop: this.picker_stop,
                     invite: '0',
                 });
+                this.clear_item();
+                this.dialog_detail = false;
             } else {
-                this.dialog_save_error = true
+                this.dialog_save_error = true;
             }
-            this.clear_item();
-            this.dialog_detail = false;
+            const getTimePromise = db.ref(`Time_teacher/${this.value}/${this.date1}`).once("value");
+            Promise.all([getTimePromise])
+                .then(([timeSnapshot]) => {
+                    if (timeSnapshot.exists()) {
+                        const timeData = timeSnapshot.val();
+                        const ed_timeData = Object.keys(timeData).map(key => key.substring(2, 7));
+                        if (this.time_standart_sum.some(time => ed_timeData.includes(time))) {
+                            alert('ใส่เวลาไม่ถูกต้องกรุณาลงใหม่อีกครั้ง');
+                        } else {
+                            if (this.mode == 'save') {
+                                db.ref(`date_teacher/${this.value}/${this.date1}/${this.picker_stop}`).set({
+                                    class: this.save_detail.class,
+                                    style_subject: this.save_detail.style,
+                                    sum_people: this.save_detail.sum_people,
+                                    invite: '0',
+                                    subject: this.save_detail.subject,
+                                    start: this.picker_start,
+                                    stop: this.picker_stop,
+                                    ID: id,
+                                });
+                                console.log(this.time_standart_sum);
+                                for (const key in this.time_standart_sum) {
+                                    if (this.save_detail.class == "Flip class") {
+                                        db.ref(`Time_teacher/${this.value}/${this.date1}/F:${this.time_standart_sum[key]}:${this.save_detail.sum_people}:${id}`).set({
+                                            0: ''
+                                        });
+                                    }
+                                    else if (this.save_detail.class == "Private") {
+                                        db.ref(`Time_teacher/${this.value}/${this.date1}/P:${this.time_standart_sum[key]}:${this.save_detail.sum_people}:${id}`).set({
+                                            0: ''
+                                        });
+                                    }
+                                };
+                                this.clear_item();
+                                this.dialog_detail = false;
+                            }
+                        }
+                    } else {
+                        if (this.mode == 'save') {
+                            db.ref(`date_teacher/${this.value}/${this.date1}/${this.picker_stop}`).set({
+                                class: this.save_detail.class,
+                                style_subject: this.save_detail.style,
+                                sum_people: this.save_detail.sum_people,
+                                invite: '0',
+                                subject: this.save_detail.subject,
+                                start: this.picker_start,
+                                stop: this.picker_stop,
+                                ID: id,
+                            });
+                            for (const key in this.time_standart_sum) {
+                                if (this.save_detail.class == "Flip class") {
+                                    db.ref(`Time_teacher/${this.value}/${this.date1}/F:${this.time_standart_sum[key]}:${this.save_detail.sum_people}:${id}`).set({
+                                        0: ''
+                                    });
+                                }
+                                else if (this.save_detail.class == "Private") {
+                                    db.ref(`Time_teacher/${this.value}/${this.date1}/P:${this.time_standart_sum[key]}:${this.save_detail.sum_people}:${id}`).set({
+                                        0: ''
+                                    });
+                                }
+                            };
+                            this.clear_item();
+                            this.dialog_detail = false;
+                        }
+                    }
+                })
         },
 
         clear_item() {
@@ -415,6 +473,9 @@ export default {
             this.hour_tea = 0;
             this.min_tea = 0;
             this.save_detail = [];
+            this.time_standart = [];
+            this.time_standart_stop = [];
+            this.time_standart_sum = [];
             this.picker_start = null;
             this.picker_stop = null;
         },
@@ -507,6 +568,7 @@ export default {
                                             sum_people: timedata.invite + "/" + timedata.sum_people,
                                             invite: timedata.invite,
                                             key: key,
+                                            IdTime: timedata.ID,
                                         });
                                         this.arrayEvents.push(date);
                                     } else { }
@@ -519,30 +581,30 @@ export default {
         },
         check_time_start() {
             const db = this.$fireModule.database();
-            db.ref(`date_teacher/${this.value}/${this.date1}`).on("value", (snapshot) => {
-                const childData = snapshot.val();
-                if (childData == null) {
-                    this.hour_tea = 0;
-                    this.min_tea = 0;
+            db.ref(`Time_teacher/${this.value}/${this.date1}`).on("value", (snapshot) => {
+                if (snapshot.exists()) {
+                    this.time_standart = [];
+                    const childData = snapshot.val();
+                    const details = Object.keys(childData).map(key => key.substring(2, 7));
+                    // this.time_standart = this.time_full.filter(time => !details.includes(time));
+                    this.time_standart = this.time_full;
+                    console.log(this.time_standart, details);
+                    // const childData = snapshot.val();
+                    // console.log(childData);
+                    // for (const key in childData) {
+                    //     const detail = key.substring(2,7);
+                    //     console.log(detail);
+                    // }
                 } else {
-                    for (const key in childData) {
-                        // console.log(key);
-                        this.hour_tea = key.substring(0, 2);
-                        this.min_tea = key.substring(3, 5);
-                    }
+                    this.time_standart = this.time_full;
                 }
+
             })
         },
 
         editItem(item) {
             console.log(item);
-            this.time_standart_stop = ["00:00", "00:30", "01:00", "01:30", "02:00", "02:30", "03:00"
-            , "03:30", "04:00", "04:30", "05:00", "05:30", "06:00", "06:30"
-            , "07:00", "07:30", "08:00", "08:30", "09:00", "09:30", "10:00"
-            , "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30"
-            , "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00"
-            , "17:30", "18:00", "18:30", "19:00", "19:30", "20:00", "20:30"
-            , "21:00", "21:30", "22:00", "22:30", "23:00", "23:30"],
+            this.time_standart_stop = this.time_full;
             this.time_standart = this.time_standart_stop;
             this.search_class_tea(item.key);
             this.search_style_tea(item.key);
@@ -568,7 +630,18 @@ export default {
         deleteItemConfirm() {
             // console.log(this.delcon);
             const db = this.$fireModule.database();
+            db.ref(`Time_teacher/${this.delcon.key}/${this.delcon.date}`).on("value", (snapshot) => {
+                const childData = snapshot.val();
+                for (const key in childData) {
+                    const detail = key.split(":");
+                    if (detail[4] == this.delcon.IdTime) {
+                        db.ref(`Time_teacher/${this.delcon.key}/${this.delcon.date}/${key}`).remove();
+                        // console.log('ลบ',key,detail[4])
+                    }
+                }
+            })
             db.ref(`date_teacher/${this.delcon.key}/${this.delcon.date}/${this.delcon.time_e}`).remove();
+
             this.delcon = [];
             this.closeDelete()
         },
