c518e45e08e1133e561d0b3cc27f78807039756a
Mr.Vongvaris Chawawiwat <62502710+attcrown@users.noreply.github.com>
2023-10-19 23:34:02 +0700

    Develop : Add_Input_register_student

--- pages/register_stu.vue
+++ pages/register_stu.vue
@@ -8,20 +8,38 @@
             <div class="text-center ">
                 <img :src="require('@/assets/fishsixLogo.png')" style="max-width: 300px;">
             </div>
+
             <v-chip style="font-size:16px;" outlined color="green"> ข้อมูลส่วนตัว </v-chip>
+
             <v-text-field v-model="dataDetail.firstName" :counter="50" :rules="nameRules" label="ชื่อจริง"
                 required></v-text-field>
             <v-text-field v-model="dataDetail.lastName" :counter="50" :rules="nameRules" label="นามสกุล"
                 required></v-text-field>
             <v-text-field v-model="dataDetail.nickname" :counter="50" :rules="nameRules" label="ชื่อเล่น"
                 required></v-text-field>
-            <v-text-field v-model="dataDetail.studentMobile" :counter="10" :rules="phoneRules" label="เบอร์โทรศัพท์"
+            <v-text-field v-model="dataDetail.studentMobile" :counter="10" :rules="phoneRules" label="เบอร์โทรศัพท์นักเรียน"
                 type="number" required></v-text-field>
-
+            <v-text-field v-model="dataDetail.parentMobile" :counter="10" :rules="phoneRules" label="เบอร์โทรศัพท์ผู้ปกครอง"
+                type="number" required></v-text-field>
             <v-text-field v-model="dataDetail.email" :rules="emailRules" label="E-mail" required></v-text-field>
-
             <v-select v-model="dataDetail.sex" :items="items" :rules="[v => !!v || 'ระบุเพศ']" label="เพศ"
                 required></v-select>
+
+            <v-chip style="font-size:16px;" outlined color="green" class="mt-3 mb-3"> ที่อยู่ปัจจุบัน </v-chip>
+            <v-text-field v-model="dataDetail.address.houseNo" :rules="addressRules" label="บ้านเลขที่"
+                required></v-text-field>
+
+            <v-combobox v-model="dataDetail.address.amphoe" clearable :items="amphoe" :rules="[v => !!v || 'ระบุข้อมูล']"
+                label="เขต/อำเภอ"></v-combobox>
+
+            <v-combobox v-model="dataDetail.address.tambon" clearable :items="tambons" :rules="[v => !!v || 'ระบุข้อมูล']"
+                label="เเขวง/ตำบล"></v-combobox>
+
+            <v-combobox v-model="dataDetail.address.province" clearable :items="province"
+                :rules="[v => !!v || 'ระบุข้อมูล']" label="จังหวัดฯ"></v-combobox>
+
+            <v-text-field v-model="dataDetail.address.zipcode" :rules="addressRules" label="รหัสไปรษณีย์" type="number"
+                required></v-text-field>
 
             <v-checkbox v-model="checkbox" :rules="[v => !!v || 'กรุณายืนยันข้อมูล']" label="ตรวจสอบข้อมูลถูกต้อง?"
                 required></v-checkbox>
@@ -29,6 +47,11 @@
                 <v-btn :disabled="!valid" color="success" class="mr-4" @click="validate">
                     ยืนยันข้อมูล
                 </v-btn>
+                <router-link to="/login">
+                    <v-btn color="amber lighten-1" class="mr-4" >                       
+                        กลับไปหน้า LOGIN
+                    </v-btn>
+                </router-link>
             </div>
         </v-form>
 
@@ -40,18 +63,18 @@
                     </v-btn>
                 </template> -->
                 <v-card>
-                    <v-card-title >
+                    <v-card-title>
                         สมัครสมาชิคสำเร็จ
                     </v-card-title>
                     <v-card-text>กรุณารอพนักงานตรวจสอบ</v-card-text>
                     <v-card-actions>
-                        <v-spacer></v-spacer>   
-                            <router-link to="/login">
-                                <v-btn color="green darken-1" text @click="dialog = false">
-                                    ตกลง
-                                </v-btn>
-                            </router-link>                   
-                        <v-spacer></v-spacer> 
+                        <v-spacer></v-spacer>
+                        <router-link to="/login">
+                            <v-btn color="green darken-1" text @click="dialog = false">
+                                ตกลง
+                            </v-btn>
+                        </router-link>
+                        <v-spacer></v-spacer>
                     </v-card-actions>
                 </v-card>
             </v-dialog>
@@ -65,7 +88,24 @@ export default {
     layout: 'register',
     data: () => ({
         dialog: false,
-        dataDetail: {},
+        dataDetail: {
+            firstName: '',
+            lastName: '',
+            nickname: '',
+            studentMobile: '',
+            email: '',
+            sex: '',
+            address: {
+                houseNo: '',
+                amphoe: '',
+                province: '',
+                tambon: '',
+                zipcode: '',
+            }
+        },
+        amphoe: [],
+        tambons: [],
+        province: [],
         valid: true,
         checkbox: null,
         phoneRules: [
@@ -80,6 +120,9 @@ export default {
             v => !!v || 'E-mail ไม่ถูกต้อง',
             v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
         ],
+        addressRules: [
+            v => !!v || 'กรุณากรอกข้อมูล'
+        ],
         items: [
             'ชาย',
             'หญิง',
@@ -87,10 +130,55 @@ export default {
         ],
     }),
 
+    mounted() {
+        this.search_amphoe();
+        this.search_tambons();
+        this.search_province();
+    },
+
     methods: {
+        search_amphoe() {
+            const db = this.$fireModule.database();
+            db.ref("RECORDS_amp/").once("value", (snapshot) => {
+                let item = [];
+                const childData = snapshot.val();
+                for (const key in childData) {
+                    item.push(childData[key].name_th);
+                }
+                console.log(item);
+                this.amphoe = item;
+            })
+        },
+        search_tambons() {
+            const db = this.$fireModule.database();
+            db.ref("RECORDS_tambons/").once("value", (snapshot) => {
+                let item = [];
+                const childData = snapshot.val();
+                for (const key in childData) {
+                    item.push(childData[key].name_th);
+                }
+                console.log(item);
+                this.tambons = item;
+            })
+        },
+        search_province() {
+            const db = this.$fireModule.database();
+            db.ref("RECORDS_city/").once("value", (snapshot) => {
+                let item = [];
+                const childData = snapshot.val();
+                for (const key in childData) {
+                    item.push(childData[key].name_th);
+                }
+                console.log(item);
+                this.province = item;
+            })
+        },
+
         validate() {
             this.$refs.form.validate()
             console.log(this.$refs.form.validate());
+
+            console.log(this.dataDetail);
             if (this.$refs.form.validate()) {
                 this.saveData(this.dataDetail);
             }
@@ -103,10 +191,10 @@ export default {
         },
         saveData(items) {
             const uniqueId = uuidv4();
-            items.status = "user";   
-            items.createdAt = new Date(); 
-            console.log(uniqueId ,items);
-            
+            items.status = "user";
+            items.createdAt = new Date();
+            console.log(uniqueId, items);
+
             const db = this.$fireModule.database()
             db.ref(`student_register/`).update({
                 [uniqueId]: items,
