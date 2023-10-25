e5217f3b23ecef5e97d8e109b52967837d1156ee
Mr.Vongvaris Chawawiwat <62502710+attcrown@users.noreply.github.com>
2023-10-25 14:30:46 +0700

    Develop : Update_submit_table

--- pages/teacher/submit_table/index.vue
+++ pages/teacher/submit_table/index.vue
@@ -1,14 +1,23 @@
 <template>
-    <div class="mx-6">        
-        <card class="mx-7"></card>
+    <div>        
+        <card></card>
+        <card_controller></card_controller>
+        <checkTeaController></checkTeaController>
+        <checkStuController></checkStuController>
     </div>
 </template>
 <script>
 import card from '~/pages/admin/submit_table/card.vue';
+import card_controller from '~/pages/admin/submit_table/card_controller.vue';
+import checkTeaController from '~/pages/admin/submit_table/checkTeaController.vue';
+import checkStuController from '~/pages/admin/submit_table/checkStuController.vue';
     export default {
         layout: 'teacherNav',
         components:{
             card,
+            card_controller,    
+            checkTeaController,
+            checkStuController,
         }
     }
 </script>
